eaf07ccfa0523b2e66c0ae8e06f637c1c327210f
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-04-25 22:58:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 28487378  # api id
+API_HASH = "8fbb5049d555f9d7db677e02a3132972"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7174903995:AAGzjuHCisBQ4AXNIDUqOe8kd9K_p7pFdiA"  # bot token
 
 
 ## REDIS
@@ -9,6 +9,6 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002115544390  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = "csrfToken=K7q0LKnyKeaz9j6YnnuAwgk9; browserid=TBBcIad85gSDEFiiX_2Sz-1plQqqfl1POXJLMdQz1MJEaQavQgispaMHP4k=; lang=en; TSID=h1mhgNtGPv7dCXz3xNVqZs0DrHgjWu13; __bid_n=18f163d54475c307924207; _ga=GA1.1.1466543190.1714065078; g_state={"i_l":0}; ndus=Yuy7MenteHuiZuVfDFAONlLzWWob0fiL8nLWmQDw; _ga_06ZNKL8C2E=GS1.1.1714065078.1.1.1714065391.60.0.0; ndut_fmt=7108F7140976389FFC4C48FB1B25B9953DF2A0A5E932D8459CA69A16CA612FEF"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [5056925533]
