b25bd7e6d29b373b8c0632ddc8a910ec8c6bcea4
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-04-25 22:50:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIE = "csrfToken=K7q0LKnyKeaz9j6YnnuAwgk9; browserid=TBBcIad85gSDEFiiX_2Sz-1plQqqfl1POXJLMdQz1MJEaQavQgispaMHP4k=; lang=en; TSID=h1mhgNtGPv7dCXz3xNVqZs0DrHgjWu13; __bid_n=18f163d54475c307924207; _ga=GA1.1.1466543190.1714065078; g_state={"i_l":0}; ndus=Yuy7MenteHuiZuVfDFAONlLzWWob0fiL8nLWmQDw; _ga_06ZNKL8C2E=GS1.1.1714065078.1.1.1714065391.60.0.0; ndut_fmt=7108F7140976389FFC4C48FB1B25B9953DF2A0A5E932D8459CA69A16CA612FEF"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [1317173146]
